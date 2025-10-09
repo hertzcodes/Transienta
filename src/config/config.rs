c71dc7fc4f4fd1008e17783cdad1180f44bf29f6
Radmehr Soleimanian <radmehr.private@gmail.com>
2025-10-09 12:52:07 +0330

    feat: manager config added example config supporting .env for config

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,11 +1,26 @@
 use serde::{Deserialize, Serialize};
 use serde_yaml;
+use std::collections::HashMap;
 use std::fs;
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
-    pub service_name: String,
+    pub manager: ManagerConfig,
     pub cache: CacheConfig,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ManagerConfig {
+    pub name: String,
+    #[serde(default = "default_manager_port")]
+    pub port: u16,
+    #[serde(default)]
+    pub shard_id: u8,
+    #[serde(default)]
+    pub neighbours: HashMap<String, String>,
+}
+
+fn default_manager_port() -> u16 {
+    return 5532;
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -18,7 +33,7 @@ pub enum CacheConfig {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct RedisConfig {
     #[serde(default)]
-    pub ssl: bool, 
+    pub tls: bool,
     #[serde(default = "default_redis_port")]
     pub port: u16,
     #[serde(default)]
@@ -32,19 +47,19 @@ pub struct RedisConfig {
 }
 
 fn default_redis_host() -> String {
-    return "127.0.0.1".to_string()
+    return "127.0.0.1".to_string();
 }
 
-
 fn default_redis_port() -> u16 {
-    return 6379
+    return 6379;
 }
 
 impl Config {
     pub fn new(path: String) -> Self {
         let content = fs::read_to_string(&path).expect(&format!("couldn't find file in {}", &path));
 
-        let config: Config = serde_yaml::from_str(&content).expect(&format!("couldn't parse config in {}", &path));
+        let config: Config =
+            serde_yaml::from_str(&content).expect(&format!("couldn't parse config in {}", &path));
 
         return config;
     }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use transienta::config::Config;
+use dotenvy::dotenv;
 use std::env;
+use transienta::config::Config;
 fn main() {
-
+    dotenv().ok();
     const DEFAULT_CONFIG_PATH: &str = "/etc/transienta/config.yaml";
     let conf_path = env::var("TRANSIENTA_CONFIG_PATH").unwrap_or(DEFAULT_CONFIG_PATH.to_string());
 
     let cfg = Config::new(conf_path);
-
 }
